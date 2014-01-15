86d6e0de4b865f1281fa4ef9d58047b713d8ef53
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-15 17:09:23 +0100

    initialer Commit

---
+++ dev/klickdummy/index.php
@@ -0,0 +1,18 @@
+<?php
+    
+	
+	$contentleftincludes = array(
+	   '',
+	   ''
+	);
+	$contentrightincludes = array(
+	   '',
+	   ''
+	);
+	$sideincludes = array(
+	   '',
+	   ''
+	);
+
+	include 'inc/tempaltes/template-2-1.inc';
+?>       
