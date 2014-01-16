82a11d00e70d208104157be35dc46c6b59b46695
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-16 16:33:00 +0100

    Footer eingefügt und Templates dementsprechend angepasst

---
+++ dev/klickdummy/impressum.php
@@ -0,0 +1,18 @@
+<?php
+    $pagetitle= "Impressum";
+	
+	$contentleftincludes = array(
+	   '',
+	   ''
+	);
+	$contentrightincludes = array(
+	   '',
+	   ''
+	);
+	$sideincludes = array(
+	   '',
+	   ''
+	);
+
+	include 'inc/templates/template-2-1.inc';
+?>
