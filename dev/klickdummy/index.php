f9eab6aa9a01fcac0b97d07eda677eed07fc3fc6
svensuch <sven.suchomel@googlemail.com>
2014-01-16 16:56:15 +0100

    da geht was

--- dev/klickdummy/index.php
+++ dev/klickdummy/index.php
@@ -2,17 +2,14 @@
     $pagetitle= "test";
 	
 	$contentleftincludes = array(
-	   '',
-	   ''
+	   'contentwidget1.inc'
 	);
 	$contentrightincludes = array(
-	   '',
-	   ''
+
 	);
 	$sideincludes = array(
-	   '',
-	   ''
-	);
 
+	);
 	include 'inc/templates/template-2-1.inc';
+	
 ?>       
