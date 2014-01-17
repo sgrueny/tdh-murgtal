16f97cbf469690b410bf25c48276e9dddc68ddd6
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-17 10:55:28 +0100

    Includes korrigiert

--- dev/klickdummy/index.php
+++ dev/klickdummy/index.php
@@ -2,12 +2,9 @@
     $pagetitle= "test";
 	
 	$contentleftincludes = array(
-<<<<<<< HEAD
-	   'contentwidget1.inc'
-=======
+
 	   'tabelle-1.inc',
-	   ''
->>>>>>> 16895f357337dec3ca8ff2fe956e90eec4978f34
+	   'contentwidget1.inc'
 	);
 	$contentrightincludes = array(
 	   'contentwidget2.inc',
