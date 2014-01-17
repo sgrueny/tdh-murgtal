299c4bf598fb37e67c3191fe50fa695d6591d4a5
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-17 19:02:47 +0100

    Seite für "Über uns" erstellt, um das Akkordeon zu demonstrieren

---
+++ dev/klickdummy/ueberuns-aktivitaeten.php
@@ -0,0 +1,18 @@
+<?php
+    $pagetitle= "Über uns - Unsere Aktivitäten";
+	$pageheadline ="Unsere Aktivitäten";
+	
+	$contentleftincludes = array(
+        'blindtext.inc',
+        'akkordeon.inc'
+	);
+	$contentrightincludes = array(
+        'contentwidget3.inc'
+	);
+	$sideincludes = array(
+		'',
+		''
+	);
+
+	include 'inc/templates/template-2-1.inc';
+?>
