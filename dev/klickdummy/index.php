89c01a952391ae28ebc90cc68ea34c8d5b78acb8
crunchman <draper@guerrillamailblock.com>
2014-01-21 22:24:00 +0100

    Navigation.css kommentiert und navigation.inc hinzugefügt

--- dev/klickdummy/index.php
+++ dev/klickdummy/index.php
@@ -1,29 +1,20 @@
 <?php
-<<<<<<< HEAD
-    $pagetitle= "test";
-=======
     $pagetitle= "Willkommen bei terre des hommes Murgtal/Mittelbaden";
-	$pageheadline ="TDH Benefizkonzert mit Peter Götzmanns Jazz Hop Rhythm";
->>>>>>> 12fe57fdc2212e3e35a0aeb0f820d32ab7c544c7
-	
-	$contentleftincludes = array(
+        $pageheadline ="TDH Benefizkonzert mit Peter Götzmanns Jazz Hop Rhythm";
+        
+        $contentleftincludes = array(
         'contentwidget1.inc'
-	);
-	$contentrightincludes = array(
+        );
+        $contentrightincludes = array(
         'blindtext.inc'
-	);
-	$sideincludes = array(
-		'leer.inc'
-	);
+        );
+        $sideincludes = array(
+                'leer.inc'
+        );
 
-<<<<<<< HEAD
-	include 'inc/templates/template-2-1.inc';
-?>       
-=======
     $widecontent = array(
-    	'sponsoren-liste.inc'
+            'sponsoren-liste.inc'
     );
 
-	include 'inc/templates/template-1-2.inc';
+        include 'inc/templates/template-1-2.inc';
 ?>
->>>>>>> 12fe57fdc2212e3e35a0aeb0f820d32ab7c544c7
