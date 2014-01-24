9b29948bc315d56efe6fb915984c63cc56819785
Björn Back <bback@students.uni-mainz.de>
2014-01-24 10:41:36 +0100

    added tabelle-2(sortable) v0.1

---
+++ dev/klickdummy/ueberuns-dasteam.php
@@ -0,0 +1,16 @@
+﻿<?php
+    $pagetitle= "Das Team - terre des hommes Murgtal/Mittelbaden";
+	
+	
+	$contentleftincludes = array(
+        'leer.inc'
+	);
+	$contentrightincludes = array(
+        'tabelle-2.inc'
+	);
+	$sideincludes = array(
+		'leer.inc'
+	);
+
+	include 'inc/templates/template-1-2.inc';
+?>
