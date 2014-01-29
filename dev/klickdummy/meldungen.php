f74d3a18efd63df6be2c9f81c5b4a03042f03088
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-29 17:26:19 +0100

    Neue Beispielseite erstellt

---
+++ dev/klickdummy/meldungen.php
@@ -0,0 +1,22 @@
+<?php
+    $pagetitle= "Willkommen bei terre des hommes Murgtal/Mittelbaden";
+        $pageheadline ="TDH Benefizkonzert mit Peter Götzmanns Jazz Hop Rhythm";
+        $topcontent = array(
+            'zusatzwidget-content-7.inc'
+        );
+        $contentleftincludes = array(
+        'contentwidget1.inc',
+        'sponsorendank.inc'
+        );
+        $contentrightincludes = array(
+		'event.inc',
+        'blindtext2.inc',
+        'blindtext.inc',
+        'blindtext3.inc'
+        );
+        $sideincludes = array(
+                'leer.inc'
+        );
+
+    include 'inc/templates/template-2-1.inc';
+?>
