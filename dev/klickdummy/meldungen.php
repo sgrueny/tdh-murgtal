1366a727600dacbc0f7c8c93c1f958afb4033d8e
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-29 18:07:43 +0100

    Neues Template für Artikel erstellt und die Artikel unfertig in die Meldungsseite neu eingebunden.

--- dev/klickdummy/meldungen.php
+++ dev/klickdummy/meldungen.php
@@ -1,19 +1,10 @@
 <?php
     $pagetitle= "Willkommen bei terre des hommes Murgtal/Mittelbaden";
-        $pageheadline ="TDH Benefizkonzert mit Peter Götzmanns Jazz Hop Rhythm";
 
-        $contentleftincludes = array(
+    $artikelincludes = array(
         'artikel1.inc',    
         'artikel2.inc'
-        );
+    );
 
-        $contentrightincludes = array(
-        'contentwidget1.inc',
-        'sponsorendank.inc'
-        );
-        $sideincludes = array(
-                'leer.inc'
-        );
-
-    include 'inc/templates/template-2-1.inc';
+    include 'inc/templates/template-artikel.inc';
 ?>
