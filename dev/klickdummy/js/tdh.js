232775f6827108c20c7586cb7d100ac98155b6e6
Jens Grochtdreis <jens@grochtdreis.de>
2014-01-30 11:03:55 +0100

    Direkt ins Akkordeon-Include eingebaute JS-Dateien entfernt und das Akkordeon-JS in die passende Datei ausgelagert.

--- dev/klickdummy/js/tdh.js
+++ dev/klickdummy/js/tdh.js
@@ -8,6 +8,8 @@ var firstAvailCol;if(typeof(matrix[rowIndex])=="undefined"){matrix[rowIndex]=[];
 
 
 $(document).ready(function() {
+	
+		// Tabellen sortieren
 		$(".tablesorter").tablesorter({sortList: [[0,0]],headers: {1: {sorter:false}}});
 
 		(function() {
@@ -22,6 +24,17 @@ $(document).ready(function() {
 			});
 		})($);
 
-		
+		// Akkordeon
+		$('.akkordeon').on('click', '.has-subnav .topic', function(e) {
+				if ($(this).next('ul').is(':visible')) {
+					$(this).next('ul').slideUp('fast');
+					$(this).removeClass('active');
+				} else {
+					$(this).closest('ul').find('.active').next('ul').slideUp('fast');
+					$(this).closest('ul').find('.active').removeClass('active');
+					$(this).next().slideToggle('fast');
+					$(this).addClass('active');
+				}
+		});
 
 });
